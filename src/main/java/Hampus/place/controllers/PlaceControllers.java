450d90eff8fd372a51cf7e023b8701fa994a66b3
Hampus Hauffman <hampus.hauffman@trustly.com>
2021-03-11 15:39:53 +0100

    jedis working

--- src/main/java/Hampus/place/controllers/PlaceControllers.java
+++ src/main/java/Hampus/place/controllers/PlaceControllers.java
@@ -1,16 +1,12 @@
 package Hampus.place.controllers;
 
-import static redis.clients.jedis.Protocol.Command.BITFIELD;
-
-import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import redis.clients.jedis.Jedis;
 
 @Controller
 public class PlaceControllers {
@@ -21,6 +17,12 @@ public class PlaceControllers {
   @SendTo("/topic/place")
   @Scheduled(fixedRate = 5000)
   public void broadcastPixels() {
+
+    //To start redis: redis-server /usr/local/etc/redis.conf
+    Jedis jedis = new Jedis("localhost");
+    jedis.bitfield("mykey","SET", "u4", "0", "10");
+    System.out.println(List.of(jedis.bitfield("mykey",  "GET", "u4", "0")));
+
 
     template.convertAndSend("/topic/place", List.of(1,2,3,4));
   }
