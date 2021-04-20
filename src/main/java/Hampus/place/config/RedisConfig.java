73cfabb48bbd74ba7f96954bffe05f13a6fc188e
HampusHauffman <hampus.hauffman@gmail.com>
2021-04-20 14:29:26 +0200

    C00l Config stuff

--- src/main/java/Hampus/place/config/RedisConfig.java
+++ src/main/java/Hampus/place/config/RedisConfig.java
@@ -3,6 +3,7 @@ package Hampus.place.config;
 import Hampus.place.Pixel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,19 +15,8 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-  @Value("${spring.redis.host}")
-  String host;
-  @Value("${spring.redis.port}")
-  int port;
-
-  @Bean
-  public JedisConnectionFactory jedisConnectionFactory(){
-    RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration(host,port);
-    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(conf);
-    jedisConnectionFactory.afterPropertiesSet();
-
-    return jedisConnectionFactory;
-  }
+  @Autowired
+  RedisConnectionFactory redisConnectionFactory;
 
   @Autowired
   @Bean
--- src/main/java/Hampus/place/redis/RedisMessageSubscriber.java
+++ src/main/java/Hampus/place/redis/RedisMessageSubscriber.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,8 @@ public class RedisMessageSubscriber implements MessageListener {
 
   public static List<String> messageList = new ArrayList<>();
 
-  private Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Pixel.class);
+  @Autowired
+  private RedisTemplate<String, Pixel> redisTemplate;
 
   @Autowired
   Webhook webhook;
@@ -27,7 +29,7 @@ public class RedisMessageSubscriber implements MessageListener {
   public void onMessage(Message message, byte[] pattern) {
 
     messageList.add(message.toString());
-    Pixel p = (Pixel) serializer.deserialize(message.getBody());
+    Pixel p = (Pixel)redisTemplate.getValueSerializer().deserialize(message.getBody());
     log.info("Message received from Redis sub: {}", p);
     webhook.sendPixel(p);
   }
