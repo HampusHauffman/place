a1ebdacb70bd5b8368e7b72c184fb209495cfea7
HampusHauffman <hampus.hauffman@gmail.com>
2021-06-17 17:28:04 +0200

    works with script

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #define MAX_ARG_PAGES 32
-curl https://place-run-qsjhjkmw7a-ew.a.run.app/board/raw -o values.txt
-redis-cli -x -h localhost -p 6379 set place < values.txt
+FILENAME=values
+TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+NAME=redisBk/$FILENAME.$TIME.txt
+touch $NAME
+
+FILENAME=$FILENAME.$TIME
+curl https://place-run-qsjhjkmw7a-ew.a.run.app/board/raw -o $NAME
+redis-cli -x -h localhost -p 6379 set place < $NAME
